3fd205ef3735b27ad69fb0006a8bd377aa766b04
Carolina1122 <71285855+Carolina1122@users.noreply.github.com>
2021-10-08 12:53:51 +0530

    Update auth.ts

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -2,6 +2,6 @@ import http from './index';
 
 const PREFIX = '/auth';
 
-export const register = (data: Record<string, any>) => http.post(`${PREFIX}/register`, data);
+export const register = (data: Record<string, any>) 
 
 export const login = (data: Record<string, any>) => http.post(`${PREFIX}/login`, data);
